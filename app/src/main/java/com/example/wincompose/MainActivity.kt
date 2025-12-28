400179f378cdc93af4d052a4a26e3ff34ead6ed6
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2025-12-28 13:30:06 +0530

    feature(produceState): implement produceState

--- app/src/main/java/com/example/wincompose/MainActivity.kt
+++ app/src/main/java/com/example/wincompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.wincompose
 
+import android.R.attr.value
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -9,9 +10,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.StopCircle
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -21,17 +32,56 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
 enum class CounterState {
     STARTED, PROGRESS, COMPLETED
+}
+
+suspend fun fetchFromNetwork(counterState: CounterState): ImageVector {
+    delay(1000)
+    return when (counterState) {
+        CounterState.STARTED -> Icons.Filled.PlayArrow
+        CounterState.PROGRESS -> Icons.Filled.Pause
+        CounterState.COMPLETED -> Icons.Filled.StopCircle
+    }
+}
+
+@Composable
+fun DisplayNetworkState(counterState: CounterState) {
+
+//    var icon by remember { mutableStateOf<ImageVector?>(null) }
+//    LaunchedEffect(key1 = counterState) {
+//        icon = fetchFromNetwork(counterState = counterState)
+//    }
+    val iconState by produceState<ImageVector?>(initialValue = null, counterState) {
+        value = fetchFromNetwork(counterState)
+    }
+
+    Box(
+        modifier = Modifier
+            .width(40.dp)
+            .height(40.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        iconState?.let {
+            Icon(
+                imageVector = it,
+                contentDescription = null,
+                modifier = Modifier.size(48.dp)
+            )
+        } ?: CircularProgressIndicator()
+    }
 }
 
 class MainActivity : ComponentActivity() {
@@ -104,6 +154,8 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
+        DisplayNetworkState(counterState)
+
         ChildComposable(
             counterMsg = counterMsg,
             context = context,
