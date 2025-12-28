92e5d02e89a098a6fd30b68573bb326a686ecd15
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2025-12-28 12:15:42 +0530

    feature(rememberUpdatedStat): implement rememberUpdatedStat

--- app/src/main/java/com/example/wincompose/MainActivity.kt
+++ app/src/main/java/com/example/wincompose/MainActivity.kt
@@ -1,55 +1,63 @@
 package com.example.wincompose
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<MainViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen(mainViewModel = viewModel)
+            MainScreen()
         }
     }
 }
 
 suspend fun startCounter(
-    increment: () -> Unit
+    increment: () -> Unit,
+    onCounterEnd: () -> Unit
 ) {
-    for (i in 1..100) {
+    for (i in 1..5) {
         delay(1000)
         increment()
     }
+    onCounterEnd()
 }
 
+
 @Composable
-fun MainScreen(mainViewModel: MainViewModel) {
+fun MainScreen() {
 
     Log.i("MainScreen", "MainScreen Invoked")
 
-    val counter by mainViewModel.counter.observeAsState()
+    var counter by remember { mutableIntStateOf(0) }
+    var counterMsg by remember { mutableStateOf("NOT STARTED YET") }
 
     val counterBgColor by remember(counter) {
         derivedStateOf {
@@ -57,16 +65,22 @@ fun MainScreen(mainViewModel: MainViewModel) {
         }
     }
 
-    LaunchedEffect(key1 = true) {
-        Log.i("MainScreen", "LaunchedEffect Invoked")
-        startCounter { mainViewModel.incrementCounter() }
+    val increment = {
+        counter++
+        counterMsg = "COUNTER IS NOW----------------: $counter"
     }
+
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
-        Log.i("MainScreen", "$counter")
+        ChildComposable(
+            counterMsg = counterMsg,
+            context = context,
+            increment = increment
+        )
 
         Text(
             text = "Counter Value: $counter",
@@ -74,6 +88,26 @@ fun MainScreen(mainViewModel: MainViewModel) {
                 .background(counterBgColor)
                 .padding(20.dp)
 
+        )
+    }
+}
+
+@Composable
+fun ChildComposable(
+    counterMsg: String,
+    context: Context,
+    increment: () -> Unit
+) {
+    val updatedCounterMsg by rememberUpdatedState(newValue = counterMsg)
+    Log.i("MainScreen", "OUTSIDE LaunchedEffect: OOOOOOOOOOOOOOO$updatedCounterMsg")
+    LaunchedEffect(key1 = true) {
+        Log.i("MainScreen", "Inside LaunchedEffect Invoked: IIIIIIIIIIIII $updatedCounterMsg")
+        startCounter(
+            increment = { increment() },
+            onCounterEnd = {
+                Log.i("MainScreen", "Count Completed $updatedCounterMsg")
+                Toast.makeText(context, "Count Completed", Toast.LENGTH_LONG).show()
+            }
         )
     }
 }
